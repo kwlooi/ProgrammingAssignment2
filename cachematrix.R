f499fa3c0765cdf0342b22a4bf80e573bcca3c69
Kok-Wai Looi <kwlooi@yahoo.com>
2014-04-27 14:45:49 -0500

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching
+## the inverse of a matrix rather than computing it repeatedly.  The following two functions
+## allow you to create a matrix, which also cache the inverse of the matrix if it is invertible.
+## This example assumes that the matrix is square and invertibel, can can be solved by using
+## the solve() function.
 
-## Write a short comment describing this function
 
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedInverse <- NULL
+    set <- function(y) {
+        x <<- y
+        cachedInverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) cachedInverse <<- inverse
+    getInverse <- function() cachedInverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    ## Computing the inverse of a square matrix can be done with the solve function in R.
+    ## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+    ## For this assignment, assume that the matrix supplied is always invertible.
+    myMatrix <- x$get()
+    inverse <- solve(myMatrix, ...)
+    x$setInverse(inverse)
+    inverse
 }
+
+## Test function to test the execution
+testSolution <- function() {
+    m <- matrix(1:4,2,2)
+    m1 <- makeCacheMatrix(m)
+    m1
+    m1$get()
+    ## debug(cacheSolve)
+    cacheSolve(m1)
+    cacheSolve(m1)
+    dim(m1)
+    m1$get()[1,]
+    m1$get()[,1]
+    m1$set(matrix (c(1,3,2,4),2,2))
+    ## undebug(cacheSolve)
+    cacheSolve(m1)
+    cacheSolve(m1)
+}
+
+
